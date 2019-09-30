eaa31e0bfb692c0e87e4c415d95be07ae84a25ff
david.moreno@evertecinc.com <david.moreno@evertecinc.com>
2019-09-30 15:55:03 -0600

    (2-LoginInitialView) Login view and animations ready

---
+++ app/src/main/java/com/davidmoreno/jetpackdemo/modules/login/viewmodel/LoginViewModel.kt
@@ -0,0 +1,2 @@
+package com.davidmoreno.jetpackdemo.modules.login.viewmodel
+
---
+++ app/src/main/java/com/davidmoreno/jetpackdemo/util/constants.kt
@@ -0,0 +1,4 @@
+package com.davidmoreno.spacenotes.common
+
+val LOADING_FRAGMENT_TAG = "LOADING_FRAGMENT"
+val DELAY: Long = 5000
